e6389e217950cfae17baabbcdad56a411de86942
Amit Thing <amittamang423@gmail.com>
2022-03-24 11:39:26 +0545

    hh

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ process.on('unhandledRejection', error => {
 
     })
 })
-
+console.log('a')
 //2. handle sigterm 
 //this happens in heroku server, because heroku will restart our app
 process.on('SIGTERM',()=>{
